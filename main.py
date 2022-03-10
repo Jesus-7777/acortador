50b66b1ce20bee93055461d490844d9509697010
Jesus-7777 <61198234+Jesus-7777@users.noreply.github.com>
2022-03-10 17:03:33 -0500

    creando enlace acortado

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fileinput import close
-from flask import Flask, render_template,request,redirect,url_for
+from flask import Flask, flash, render_template,request,redirect,url_for
 import mysql.connector
 import random
 import string
@@ -23,9 +23,8 @@ def index():
     #cursor.execute('select id from enlaces')
     #cursor.execute('select exten from enlaces')
     #cursor.execute('select cort from enlaces')
-    
+
     productos=cursor.fetchall()
-    
     cursor=close()
     return render_template("index.html",productos=productos)
 
@@ -36,24 +35,28 @@ def url_corto():
         rand_letters = "".join(rand_letters)
         return rand_letters
 
-@app.post("/")
+@app.route("/crear",methods=["GET","POST"])
 def crearUrlPost():
-    exten_url=request.form.get('exten')
-    short_url = url_corto()
-    print(short_url)
     cursor=db.cursor()
-    cursor.execute("insert into enlaces(exten,cort) values(%s,%s) ",(exten_url,url_corto()))
-    cursor.close()
-    
-    return redirect(url_for('index'))
+    if request.method=='POST':
+        exten_url=request.form.get('exten')
+        corto=url_corto()
+        cursor.execute("insert into enlaces(exten,cort) values(%s,%s) ",(exten_url,corto))
+        #cursor.execute("insert into enlaces(exten) values('%s')"%exten_url)
 
-@app.get('/<urlId>')
+        cursor.close()
+        
+    return render_template("ventanas/crear.html",corto=corto)
+    #return render_template("index.html",corto=corto)
+
+@app.get('/short/<urlId>')
 def traerUrl(urlId):
     cursor=db.cursor()
     cursor.execute("SELECT exten FROM enlaces WHERE cort=%(cort)s",{'cort':urlId})
     productos=cursor.fetchone()
     cursor.close()
-    #return productos[0]
-    return render_template("/index.html", contactoId = productos[0])
+    return redirect(productos[0])
+    #return render_template("index.html", contactoId = productos[0])
+    #return render_template(redirect('index', contactoId = productos[0]))
 
 app.run(debug=True)
